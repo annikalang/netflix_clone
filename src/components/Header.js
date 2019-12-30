703bc4c970ec5a28904119b8e7d342010d16e366
Annika Lang <Lang-annika@t-online.de>
2019-12-30 16:29:59 +0100

    added header-container div

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Header</h1>
+      <div className="header-container">
+
       </div>
-      )
+      );
   }
 }
 
