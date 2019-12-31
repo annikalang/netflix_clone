7558391062c1d80b159b7a98607190df500e67cc
Annika Lang <Lang-annika@t-online.de>
2019-12-31 17:50:29 +0100

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 // importing react
 import React, { Component } from "react";
 
+// importing logo
+import logo from "../svg/logo.svg"
+
 class Header extends Component {
   render() {
     return (
       <div className="header-container">
         <div className="header-top">
-
+          <img src={logo} />
         </div>
       </div>
       );
