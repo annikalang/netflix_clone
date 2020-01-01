5cc7e5f77033b6031c7e87dc68991bca1a67447d
Annika Lang <Lang-annika@t-online.de>
2020-01-01 22:23:47 +0100

    importing react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ import React, { Component } from "react";
 // importing logo
 import logo from "../svg/logo.svg"
 
+// importing react router
+import { NavLink } from "react-router-dom"
+
 class Header extends Component {
   render() {
     return (
