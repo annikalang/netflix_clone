41973ff14716f97c70910667e14453a11cc20370
Annika Lang <Lang-annika@t-online.de>
2020-01-11 23:34:57 +0100

    tried to fix bug with semicolon and moving the style code

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,30 @@
 import React, { Component } from "react";
 
 // importing logo
-import logo from "../svg/logo.svg"
+import logo from "../svg/logo.svg";
 
 // importing react router
 import { NavLink } from "react-router-dom";
 
 // importing styled components
 import styled from "styled-components";
+
+// Header Container
+const HeaderContainer = styled.div
+  .signIn-btn; {
+    right: 0;
+    margin: 1.125rem 3% 0;
+    padding: 0.4375rem 1.0635rem;
+    font-weight: 400;
+    line-height: normal;
+    border-radius: 0.1875rem;
+    font-size: 1 rem;
+    background: var(--main-red);
+    position: absolute;
+    translate: transform(-50%, -50%);
+    cursor: pointer;
+    transition: background 0.2s ease-in;
+  };
 
 class Header extends Component {
   render() {
@@ -20,25 +37,8 @@ class Header extends Component {
         </div>
       </HeaderContainer>
       );
-  }
-}
+  };
+};
 
 export default Header;
 
-// Header Container
-
-const HeaderContainer= styled.headers
-  .signIn-btn {
-    right: 0;
-    margin: 1.125rem 3% 0;
-    padding: 0.4375rem 1.0635rem;
-    font-weight: 400;
-    line-height: normal;
-    border-radius: 0.1875rem;
-    font-size: 1rem;
-    background: var(--main-red);
-    position: absolute;
-    translate: transform(-50%, -50%);
-    cursor: pointer;
-    transition: background 0.2s ease-in;
-  }
